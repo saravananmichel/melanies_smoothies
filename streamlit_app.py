64e62d4c63a2c6f206570db974da3476db59f410
saravananmichel <jobs.4.saravanan@gmail.com>
2025-11-03 17:37:05 +0800

    Update streamlit_app.py
    session.use_warehouse("COMPUTE_WH")

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.write('The name on your Smoothie will be', name_on_order)
 
 cnx=st.connection("snowflake")
 session = cnx.session()
-session.use_warehouse("COMPUTE_CH")
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe,use_container_width=True)
 
@@ -40,6 +40,7 @@ if ingredients_List:
   
     session.use_database("SMOOTHIES")
     session.use_schema("PUBLIC")
+    session.use_warehouse("COMPUTE_WH")
   
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
